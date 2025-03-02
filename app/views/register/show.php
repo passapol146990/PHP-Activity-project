540644ad49213d5fe75b1d9e750e8bee5c07020f
Passapol <phasphlsuthahthrrm976@gmail.com>
2025-03-03 01:51:14 +0700

    Update show.php

--- app/views/register/show.php
+++ app/views/register/show.php
@@ -8,7 +8,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
-    <title>req Activity</title>
+    <title>กิจกรรมที่เข้าร่วม</title>
 
     <style>
        .text-font {
