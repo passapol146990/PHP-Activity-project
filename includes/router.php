79794f025b2b601689e02e50b1e135c61d9f4e96
POOMIX <92581505+POOMIX@users.noreply.github.com>
2025-03-03 00:55:24 +0700

    updated

--- includes/router.php
+++ includes/router.php
@@ -121,7 +121,6 @@ if($method=="GET"){
                 $posts = getPostBySearch(10,$page,$seach);
             }else{
                 $posts = getPost(10,$page);
-                print_r($posts);
             }
             require_once('../app/views/home.php');
             exit();
@@ -145,7 +144,6 @@ if($method=="GET"){
             $aid = $_SESSION["login_token"];
             $page = $_GET['page'] ?? 1;
             $myactivities = getRegisteredActivities($aid,10,$page);
-            print_r($myactivities);
             require_once('../app/views/register/show.php');
             exit();
             break;
@@ -188,7 +186,6 @@ if($method=="GET"){
             $id_user = $_SESSION["login_token"];
             $page = $_GET['page'] ?? 1;
             $data = getPostUserCreate($id_user,10,$page);
-            print_r($data);
             if($data["status"]!=200){
                 $data["data"] = [];
             }
